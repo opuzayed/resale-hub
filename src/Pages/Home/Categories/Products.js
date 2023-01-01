7b612ca63c2bfe1c50a6a25628af2f7e69eca4d8
opuzayed <zayedopu4933@gmail.com>
2023-01-02 02:12:45 +0600

    show modal by clicking book now button

--- src/Pages/Home/Categories/Products.js
+++ src/Pages/Home/Categories/Products.js
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import BookingModal from './BookingModal/BookingModal';
 import ProductsCard from './ProductsCard';
 const Products = () => {
+
     const Productsitems = useLoaderData();
+    const [singleProduct, setSingleProduct] = useState(null);
+
     const items = Productsitems.category_items;
    
     return (
-        <div className='grid gap-6 grid-cols-1 md:grid-cols-2 m-8 justify-center'>
+        <section>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 m-8 justify-center'>
             {
                 items.map(item => <ProductsCard 
-                key={item._id}
-                item={item}
+                    key={item._id}
+                    item={item}
+                    setSingleProduct = {setSingleProduct}
                 ></ProductsCard>)
             }
-        </div>
+            </div>
+           { 
+            singleProduct &&
+           <BookingModal 
+            singleProduct = {singleProduct}
+            ></BookingModal> }
+        </section>
     );
 };
 
--- src/Pages/Home/Categories/ProductsCard.js
+++ src/Pages/Home/Categories/ProductsCard.js
@@ -1,8 +1,7 @@
-import { Button, Card } from "flowbite-react";
+import { Card } from "flowbite-react";
 import React from "react";
 
-const ProductsCard = ({ item }) => {
-
+const ProductsCard = ({ item, setSingleProduct }) => {
   const {
     image_url,
     name,
@@ -16,10 +15,7 @@ const ProductsCard = ({ item }) => {
   return (
     <div>
       <div className="max-w-sm">
-        <Card
-          imgAlt="laptop pic"
-          imgSrc={image_url}
-        >
+        <Card imgAlt="laptop pic" imgSrc={image_url}>
           <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             ProductName : {name}
           </h5>
@@ -39,12 +35,9 @@ const ProductsCard = ({ item }) => {
             Date : {date}
           </p>
           <div>
-        <Button gradientDuoTone="tealToLime">
-          Book Now
-        </Button>
-      </div>
+            <label htmlFor="booking-modal" onClick={()=>setSingleProduct(item)} className="btn text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Book Now</label>
+          </div>
         </Card>
-        
       </div>
     </div>
   );
