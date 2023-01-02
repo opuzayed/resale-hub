b2cba30980df99de51166ef674e4aa461b5f2e23
opuzayed <zayedopu4933@gmail.com>
2023-01-03 02:07:08 +0600

    basic login structure done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div className='h-[800px] flex justify-center items-center'>
-            <div>
-                <h2 className='text-4xl'>Please Login</h2>
-            </div>
-        </div>
-    );
+  const { register, handleSubmit } = useForm();
+  
+  const handleLogin = data => 
+  {
+    console.log(data);
+  }
+
+  return (
+    <div className="h-[800px] flex justify-center items-center">
+      <div className="w-96 p-7">
+        <h2 className="text-4xl text-center">Please Login</h2>
+        <form onSubmit={handleSubmit(handleLogin)}>
+          <div className="form-control w-full max-w-xs">
+            <label className="label"> <span className="label-text">Email</span> </label>
+            <input type="email" className="input input-bordered w-full max-w-xs" {...register("email")} />
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label"> <span className="label-text">Password</span> </label>
+            <input type="password" className="input input-bordered w-full max-w-xs" {...register("password")} />
+            <label className="label"> <span className="label-text">Forget Password?</span> </label>
+          </div>
+          <input className="btn w-full text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" type="submit" value='Login'/>
+        </form>
+        <p>New to Computers Hub?<Link className="text-sky-400" to='/signup'> Create new account</Link></p>
+        <div className="divider">OR</div>
+        <button className="btn btn-outline btn-accent w-full">CONTINUE WITH GOOGLE</button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
