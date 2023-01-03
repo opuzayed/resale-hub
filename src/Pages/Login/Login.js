e3aa94ece77aec760c675746ef1b4c528effab89
opuzayed <zayedopu4933@gmail.com>
2023-01-03 21:18:00 +0600

    validation added in login form

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const Login = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, formState: { errors }, handleSubmit } = useForm();
   
   const handleLogin = data => 
   {
@@ -17,11 +17,16 @@ const Login = () => {
         <form onSubmit={handleSubmit(handleLogin)}>
           <div className="form-control w-full max-w-xs">
             <label className="label"> <span className="label-text">Email</span> </label>
-            <input type="email" className="input input-bordered w-full max-w-xs" {...register("email")} />
+            <input type="email" className="input input-bordered w-full max-w-xs" {...register("email", {required : "Email Address is required" })}/>
+            {errors.email && <p className="text-error">{errors.email?.message}</p>}
           </div>
           <div className="form-control w-full max-w-xs">
             <label className="label"> <span className="label-text">Password</span> </label>
-            <input type="password" className="input input-bordered w-full max-w-xs" {...register("password")} />
+            <input type="password" className="input input-bordered w-full max-w-xs" {...register("password", {required : 'Password is required'
+          ,
+          minLength:{value : 6, message:'Password should be six characters or long'}
+          })}/>
+            {errors.password && <p className="text-error">{errors.password?.message}</p>}
             <label className="label"> <span className="label-text">Forget Password?</span> </label>
           </div>
           <input className="btn w-full text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" type="submit" value='Login'/>
