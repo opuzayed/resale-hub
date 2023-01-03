c468bab8d1b5173c1eda5da34001c192f0fa21e1
opuzayed <zayedopu4933@gmail.com>
2023-01-04 02:28:28 +0600

    sign up page implement

--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -1,8 +1,17 @@
 import { Avatar, Dropdown, Navbar, Tooltip } from "flowbite-react";
+import { useContext } from "react";
 import laptop from "../../assets/Images/laptop.png";
 import ThemeSwitcher from "../../component/ThemeSwitcher/ThemeSwitcher";
+import { AuthContext } from "../../contexts/AuthProvider";
 
 const Header = () => {
+        const {user, logOut} = useContext(AuthContext);
+
+        const handleLogOut = () => {
+          logOut()
+          .then(()=> {})
+          .catch(error => console.error(error));
+        }
 
   return (
     <div>
@@ -52,11 +61,17 @@ const Header = () => {
           <Navbar.Link href="/navbars">Pricing</Navbar.Link>
           <Navbar.Link href="/navbars">Contact</Navbar.Link>
           <Navbar.Link href="/signup">
-          <button className="btn text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">SignUp</button>
+          <button className="btn text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Sign up</button>
           </Navbar.Link>
+          { user?.uid ?
+            <Navbar.Link href="/login">
+            <button onClick={handleLogOut} className="btn text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Sign out</button>
+          </Navbar.Link>
+          :
           <Navbar.Link href="/login">
-          <button className="btn text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Login</button>
+            <button className="btn text-gray-900 bg-gradient-to-r from-teal-200 to-lime-200 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-teal-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Log in</button>
           </Navbar.Link>
+          }
          
         </Navbar.Collapse>
       </Navbar>
--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,11 +1,13 @@
-import React, { createContext } from 'react';
-import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword} from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut} from 'firebase/auth';
 import app from '../Firebase/Firebase.config';
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
+
+    const [user, setUser] = useState(null);
 
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
@@ -15,10 +17,25 @@ const AuthProvider = ({children}) => {
         return signInWithEmailAndPassword(auth, email, password);
     }
 
+    const logOut = () => {
+        return signOut(auth);
+    }
+
+    useEffect(() => {
+            const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('user observing');
+            setUser(currentUser);
+          });
+            return () => unsubscribe();
+    }, [])
+
     const authInfo = 
     {
         createUser,
-        signIn
+        signIn,
+        logOut,
+        user,
+        
     }
     return (
         <AuthContext.Provider value={authInfo}>
